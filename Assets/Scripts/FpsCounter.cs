f73996f221d816eabb47558dec154015683cc961
Ryota Yokote <yokote.ryota@donuts.ne.jp>
2016-10-04 12:48:53 +0900

    first

---
+++ Assets/Scripts/FpsCounter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class FpsCounter : MonoBehaviour {
+  private Text text;
+
+  private int frameCount;
+  private float prevTime;
+
+  void Start() {
+    Application.targetFrameRate = 60;
+
+    text = GetComponent<UnityEngine.UI.Text> ();
+
+    frameCount = 0;
+    prevTime = 0.0f;
+  }
+
+  void Update() {
+    
+    ++frameCount;
+    float time = Time.realtimeSinceStartup - prevTime;
+
+    if (time >= 0.5f) {
+      text.text = (frameCount / time).ToString ("F1");
+
+      frameCount = 0;
+      prevTime = Time.realtimeSinceStartup;
+    }
+
+  }
+}
