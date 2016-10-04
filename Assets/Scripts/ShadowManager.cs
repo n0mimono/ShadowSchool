b812ace80a4e6ff67244f2fa7dc08096d01e5bf5
Ryota Yokote <yokote.ryota@donuts.ne.jp>
2016-10-04 18:12:38 +0900

    全て追加

--- Assets/Scripts/ShadowManager.cs
+++ Assets/Scripts/ShadowManager.cs
@@ -13,22 +13,45 @@ public class ShadowManager : MonoBehaviour {
   [Header("Objects")]
   public Transform cameraTrans;
 
+  [Header("Mesh")]
+  public List<Mesh> meshes;
+
+  public GameObject cubeBase;
+  public GameObject planeBase;
+
+  public GameObject caster;
+  public GameObject receiver;
+
+  public List<GameObject> cubeManies;
+  public List<GameObject> planeManies;
+  public GameObject planeOut;
+
   // screen data
-  private int scrWidth;
-  private int scrHeight;
+  private Resolution scrRes;
   private int curRes = 0;
   private float[] res = { 1f, 0.5f, 0.25f }; 
+
+  // mesh mode
+  private int curMesh = 0;
+
+  // scale mode
+  private int curCubeScale = 0;
+  private int curPlaneScale = 0;
+  private float[] scl = { 1f, 2f, 0.5f };
+
+  // many
+  private int curCubeMany = 0;
+  private int curPlaneMany = 0;
 
   void Start() {
     Application.targetFrameRate = 60;
 
-    scrWidth = Screen.width;
-    scrHeight = Screen.height;
+    scrRes = Screen.currentResolution;
   }
 
   public void ChangeResolution() {
     curRes = (curRes + 1) % 3;
-    Screen.SetResolution ((int)(scrWidth * res [curRes]), (int)(scrHeight + res [curRes]), true);
+    //Screen.SetResolution ((int)(scrRes.width * res [curRes]), (int)(scrRes.height * res [curRes]), true);
   }
 
   public void ChangeShadow() {
@@ -42,27 +65,60 @@ public class ShadowManager : MonoBehaviour {
   }
 
   public void SwitchCubeBase() {
+    cubeBase.SetActive (!cubeBase.activeInHierarchy);
   }
 
   public void SwitchCubeMany() {
+    curCubeMany = (curCubeMany + 1) % 4;
+
+    cubeManies.ForEach (m => m.SetActive (false));
+    if (curCubeMany != 0) {
+      cubeManies [curCubeMany - 1].SetActive (true);
+    }
   }
 
   public void SwitchCubeScale() {
+    curCubeScale = (curCubeScale + 1) % 3;
+
+    Transform[] transes = caster.GetComponentsInChildren<Transform> (true);
+    foreach (Transform trans in transes) {
+      trans.localScale = Vector3.one * scl[curCubeScale];
+    }
   }
 
   public void SwitchSpehre() {
+    curMesh = (curMesh + 1) % 2;
+
+    MeshFilter[] filters = caster.GetComponentsInChildren<MeshFilter> (true);
+    foreach (MeshFilter filter in filters) {
+      filter.sharedMesh = meshes [curMesh];
+    }
   }
 
   public void SwitchPlaneBase() {
+    planeBase.SetActive (!planeBase.activeInHierarchy);
   }
 
   public void SwitchPlaneMany() {
+    curPlaneMany = (curPlaneMany + 1) % 4;
+
+    planeManies.ForEach (m => m.SetActive (false));
+    if (curPlaneMany != 0) {
+      planeManies [curPlaneMany - 1].SetActive (true);
+    }
   }
 
   public void SwitchPlaneScale() {
+    curPlaneScale = (curPlaneScale + 1) % 3;
+
+    Transform[] transes = receiver.GetComponentsInChildren<Transform> (true);
+    foreach (Transform trans in transes) {
+      trans.localScale = Vector3.one * scl[curPlaneScale];
+    }
   }
 
   public void SwitchPlaneOut() {
+    planeOut.SetActive (!planeOut.activeInHierarchy);
   }
 
   public void ShowPanel() {
