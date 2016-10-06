ded034799ce8dede2db16859b42977d9e638435d
Ryota Yokote <yokote.ryota@donuts.ne.jp>
2016-10-06 18:09:59 +0900

    ダミー追加

---
+++ Assets/Scripts/ShadowProjector.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class ShadowProjector : MonoBehaviour {
+  [Header("Light Option")]
+  public Transform dirLight;
+  public float     distance;
+
+  [Header("Shadow Map Options")]
+  public int width;
+  public int height;
+  public RenderTexture shadowMap;
+  public Shader        replacedShader;
+
+  [Header("Projector Options")]
+  public Projector projector;
+  public Material  projMat;
+
+  private Camera camera;
+
+  void LateUpdate() {
+    if (shadowMap == null) CreateShadowMap ();
+
+    // auto-tranform
+    transform.forward = dirLight.forward;
+    transform.position = -1f * transform.forward * distance;
+
+    // auto-set projector
+    projector.orthographicSize = camera.orthographicSize;
+  }
+
+  private void CreateShadowMap() {
+    // create shadow map
+    shadowMap = new RenderTexture (width, height, 0, RenderTextureFormat.ARGB32);
+    shadowMap.name = "CustomShadowMap_" + name;
+
+    // set camera and rendering settings
+    camera = GetComponent<Camera> ();
+    camera.depthTextureMode = DepthTextureMode.Depth;
+    camera.targetTexture = shadowMap;
+    camera.SetReplacementShader(replacedShader, "");
+
+    // set projection material
+    //projMat = Instantiate(projMat);
+    //projMat.SetTexture ();
+    //projector.material = projMat;
+  }
+
+}
---
+++ Assets/Shaders/SimpleEncoder.shader
@@ -0,0 +1,28 @@
+﻿Shader "SimpleEncoder" {
+  SubShader {
+    Tags {"RenderType"="Opaque"}
+
+    Pass {
+      CGPROGRAM
+      #pragma vertex vert
+      #pragma fragment frag
+      #include "UnityCG.cginc"
+
+      struct v2f {
+        float4 pos : SV_POSITION;
+      };
+
+      v2f vert (appdata_full v) {
+        v2f o;
+        o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+        return o;
+      }
+      
+      fixed4 frag (v2f i) : SV_Target {
+        return float4(1,1,1,1);
+      }
+      ENDCG
+
+    }
+  }
+}
