617fb879d2f4077c4864dfcd2f51014c01a0ed95
Ryota Yokote <yokote.ryota@donuts.ne.jp>
2016-10-07 10:38:25 +0900

    検証シーンをさらに追加

---
+++ Assets/Scripts/Tool/UiTtool.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UiTtool : MonoBehaviour {
+
+  [ContextMenu("Remove Raycast Target From All Texts")]
+  void RemoveRaycastTargetFromAllTexts() {
+    foreach (Text text in FindObjectsOfType<Text>()) {
+      text.raycastTarget = false;
+    }
+  }
+
+}
